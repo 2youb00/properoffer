b20927740086b7501eac9a1169fcfe61486fea62
xovzd <144159255+2youb00@users.noreply.github.com>
2023-10-28 13:28:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,84 @@
+psids = [];
+psshowhidde = [];
+divslength=0;
+let faqBars = document.querySelectorAll('.faqbars');
+faqBars.forEach(function (faqBar, index) {
+    for (let i = 0; i <= index; i++) {
+        psids[index] = i;
+        psshowhidde[index] = true;
+        divslength++;
+    }
+});
+
+
 let showhidde=true;
 console.log("1");
     function HandS(faqbars) {
-        console.log("hhh2h");
-        let closep = faqbars.closest('p');
-        if(showhidde){
+        let index = Array.from(document.querySelectorAll('.faqbars')).indexOf(faqbars);
+        let divindex=0;
+        for(let i=0;i<=divslength;i++){
+            if(psids[i]==index){
+                divindex=i;
+            }
+        }
+        let closep = faqbars.closest('.faqbars').querySelector('p');
+        let closeh3 = faqbars.closest('.faqbars').querySelector('h3');
+        let closedown = faqbars.closest('.faqbars').querySelector('i');
+        
+        if(psshowhidde[divindex]){
             closep.classList.remove("displaynone");
-            showhidde=false;
+            psshowhidde[divindex]=false;
+            closedown.outerHTML='<i class="fa-solid fa-angle-up"></i>';
+            closedown.style.color='#41bfc6';
+            closeh3.style.color='#41bfc6';
         }
         else{
             closep.classList.add("displaynone");
-            showhidde=true;
+            psshowhidde[divindex]=true;
+            closedown.outerHTML='<i class="fa-solid fa-angle-down"></i>';
+            closedown.style.color='#111837';
+            closeh3.style.color='#111837';
         }
     }
 
 
 
-
-
-
-
-
 /*
-.closest():
-let faqps=document.querySelectorAll(".faqbars p");
-let faqbars=document.querySelectorAll(".faqbars");
-let showhidde=true;
-faqbars.forEach(faqbar => {
-    faqbar.addEventListener("click",()=>{
-        faqps.forEach(faqp => {
-            if(showhidde){
-                faqp.classList.remove("displaynone");
-                showhidde=false;
-            }
-            else{
-                faqp.classList.add("displaynone");
-                showhidde=true;
-            }
-        });
-    })
+psids = [];
+psshowhidde = [];
+divslength=0;
+let faqBars = document.querySelectorAll('.faqbars');
+faqBars.forEach(function (faqBar, index) {
+    for (let i = 0; i <= index; i++) {
+        psids[index] = i;
+        psshowhidde[index] = true;
+        divslength++;
+    }
 });
+console.log("psids:",psids);
 
 
-
-
-
-
-
-
-
-
+let showhidde=true;
+console.log("1");
+    function HandS(faqbars) {
+        let divindex=0;
+        for(let i=0;i<=divslength;i++){
+            if(psids[i]==faqbars.index){
+                divindex=i;
+            }
+        }
+        console.log("inex:",faqbars.index);
+        let closep = faqbars.closest('.faqbars').querySelector('p');
+        if(psshowhidde[divindex]){
+            closep.classList.remove("displaynone");
+            psshowhidde[divindex]=false;console.log("if");
+        }
+        else{
+            closep.classList.add("displaynone");
+            psshowhidde[divindex]=true;
+            console.log("else");
+        }
+        console.log(psshowhidde[divindex]);
+    }
 
 /*/
